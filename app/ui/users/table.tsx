4109c813484c546deba27582709f80f62618f69f
Hiro <Hiro.work1011@gmail.com>
2025-05-16 19:25:05 +0900

    userの削除機能の追加

--- app/ui/users/table.tsx
+++ app/ui/users/table.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/users/buttons';
+import { UpdateInvoice, DeleteUser } from '@/app/ui/users/buttons';
 import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/lib/utils';
 import { fetchFilteredUsers } from '@/lib/data';
@@ -48,7 +48,7 @@ export default async function UsersTable({
                   </div> */}
                   <div className="flex justify-end gap-2">
                     <UpdateInvoice id={user.id} />
-                    <DeleteInvoice id={user.id} />
+                    <DeleteUser id={user.id} />
                   </div>
                 </div>
               </div>
@@ -110,7 +110,7 @@ export default async function UsersTable({
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex justify-end gap-3">
                       <UpdateInvoice id={user.id} />
-                      <DeleteInvoice id={user.id} />
+                      <DeleteUser id={user.id} />
                     </div>
                   </td>
                 </tr>
--- lib/actions.ts
+++ lib/actions.ts
@@ -128,8 +128,11 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+
+export async function deleteUser(id: string) {
+  await sql`DELETE FROM users WHERE id = ${id}`;
+  revalidatePath('/dashboard/users');
 }
